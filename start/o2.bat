117f98c602dc3796f044857bf7894a8b61078cd7
jjfnext <jjfnext@gmail.com>
2015-10-22 14:03:01 -0400

    add bat

---
+++ start/o2.bat
@@ -0,0 +1,4 @@
+@echo off
+@echo sublime2
+start "" "C:\Program Files\Sublime Text 2\sublime_text.exe" --new-window %1
+
---
+++ start/o3.bat
@@ -0,0 +1,4 @@
+@echo off
+@echo sublime3
+start "" "C:\Program Files\Sublime Text 3\sublime_text.exe" --new-window %1
+
